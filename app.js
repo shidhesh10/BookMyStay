95368fd8bea668d6eea145e93c5c0d0dec4d6ad7
shidhesh10 <shidheshbhatiya1004@gmail.com>
2025-11-06 12:43:07 +0530

    Added frontend UI files - EJS templates and public CSS

--- app.js
+++ app.js
@@ -2,6 +2,18 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const Listing = require("./models/listing.js")
+const path = require("path");
+const methodOverride = require("method-override")
+const ejsMate = require("ejs-mate");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"))
+
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "/public")));
+
+app.engine('ejs', ejsMate);
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/BookMyStay";
 
@@ -18,21 +30,56 @@ app.get("/", (req, res) => {
   res.send("✅ Express is working perfectly!");
 });
 
-app.get("/testlisting", async (req, res) => {
-  let sampleListing = new Listing({
-    title: "Bali Trip",
-    description: "Sitting by the beach",
-    price: 1200,
-    location:"Bali",
-    country: "Unknown",
-
-  });
-
-  await sampleListing.save();
-  console.log("Sample was saved");
-  res.send("Successful");
+//Index route
+app.get("/listings", async (req,res) => {
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs", {allListings})
 })
 
+//New Route
+app.get("/listings/new", (req, res) => {
+  res.render("listings/new.ejs")
+})
+
+//Show Route
+app.get("/listings/:id",async (req, res) => {
+  let {id} = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/show.ejs", {listing})
+})
+
+//Create Route
+app.post("/listings", async (req, res) => {
+  const newListing = new Listing(req.body.listing);
+  await newListing.save();
+  res.redirect("/listings");
+})
+
+//Edit Route
+app.get("/listings/:id/edit", async (req, res) => {
+  let {id} = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listings/edit.ejs", {listing})
+
+})
+
+//Update Route
+app.put("/listings/:id", async (req, res) => {
+  let {id} = req.params;
+  await Listing.findByIdAndUpdate(id, {...req.body.listing});
+  res.redirect(`/listings/${id}`);
+})
+
+//Delete Route
+app.delete("/listings/:id", async (req, res) => {
+  let {id} = req.params;
+  let deletedListing = await Listing.findByIdAndDelete(id);
+  res.redirect("/listings");
+  
+})
+
+
+
 app.listen(8080, () => {
-  console.log("Server started: http://localhost:8080");
+  console.log("Server is listening to port 8080");
 });
