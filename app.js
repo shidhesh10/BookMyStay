548e164c15e8a068cbed66f406cb68ce61a08d3d
shidhesh10 <shidheshbhatiya1004@gmail.com>
2025-11-16 20:17:56 +0530

    adding error handeling

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const path = require("path");
 const methodOverride = require("method-override")
 const ejsMate = require("ejs-mate");
 
+app.engine('ejs', ejsMate);
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"))
 
@@ -13,7 +15,7 @@ app.use(express.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "/public")));
 
-app.engine('ejs', ejsMate);
+
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/BookMyStay";
 
---
+++ public/js/script.js
@@ -0,0 +1,35 @@
+// // Example starter JavaScript for disabling form submissions if there are invalid fields
+// (() => {
+//   'use strict'
+
+//   // Fetch all the forms we want to apply custom Bootstrap validation styles to
+//   const forms = document.querySelectorAll('.needs-validation')
+
+//   // Loop over them and prevent submission
+//   Array.from(forms).forEach(form => {
+//     form.addEventListener('submit', event => {
+//       if (!form.checkValidity()) {
+//         event.preventDefault()
+//         event.stopPropagation()
+//       }
+
+//       form.classList.add('was-validated')
+//     }, false)
+//   })
+// })
+
+(() => {
+  'use strict';
+  const forms = document.querySelectorAll('.needs-validation');
+
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault();   // stop page reload
+        event.stopPropagation();  // stop form submission
+      }
+
+      form.classList.add('was-validated');
+    }, false);
+  });
+})();
