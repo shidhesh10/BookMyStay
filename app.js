57f6008415047e446e44663a9bee82b38ace8838
shidhesh10 <shidheshbhatiya1004@gmail.com>
2025-12-14 17:38:57 +0530

    feat: implement complete authentication system with flash feedback
    - Integrate Passport.js for secure Local Strategy authentication
    - Add Signup and Login routes with proper error handling
    - Implement Flash messages for success ('Welcome back') and failure ('Invalid credentials')
    - Secure password storage using hashing and salting (passport-local-mongoose)

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
 
 const sessionOptions = {
   secret: "mysupersecretcode",
@@ -30,8 +33,9 @@ app.use(express.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "/public")));
 
-const listings = require("./routes/listing.js");
-const reviews = require("./routes/review.js");
+const listingRouter = require("./routes/listing.js");
+const reviewRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js")
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/BookMyStay";
 
@@ -51,6 +55,13 @@ app.get("/", (req, res) => {
 app.use(session(sessionOptions));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req, res, next) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
@@ -58,8 +69,20 @@ app.use((req, res, next) => {
 })
 
 
-app.use("/listings", listings);
-app.use("/listings/:id/reviews", reviews);
+// app.get("/demouser", async (req, res) => {
+//   let fakeUser = new User({
+//     email: "student@gmail.com",
+//     username: "delta-student"
+//   });
+
+//   const registeredUser = await User.register(fakeUser, "helloworld");
+//   res.send(registeredUser);
+// })
+
+
+app.use("/listings", listingRouter);
+app.use("/listings/:id/reviews", reviewRouter);
+app.use("/", userRouter)
 
 app.all(/.*/,(req, res, next) => {
   next(new ExpressError(404, "Page Not Found!"));
