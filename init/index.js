cbda0526489393b1aab46599d93ee110796ad3ab
shidhesh10 <shidheshbhatiya1004@gmail.com>
2025-12-15 23:05:43 +0530

    feat: implement user authorization and dynamic UI states
    - Implemented conditional rendering for the Navbar (Login vs Logout states).
    - Updated Listing Schema to associate listings with specific owners.
    - Added authorization middleware: Users can now only edit/delete their own listings.
    - Hidden 'Edit' and 'Delete' buttons on the UI for non-owners.
    - Added owner attribution to the listing details page.

--- init/index.js
+++ init/index.js
@@ -15,6 +15,7 @@ async function main() {
 
 const initDB = async () => {
     await Listing.deleteMany({});
+    initData.data = initData.data.map((obj) => ({...obj, owner: "693ff47d9670c1177f420ffb"}));
     await Listing.insertMany(initData.data);
     console.log("Data was initialize");
 }
--- routes/listing.js
+++ routes/listing.js
@@ -4,6 +4,7 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const ExpressError = require("../utils/ExpressError.js");
 const {listingSchema} = require("../schema.js");
 const Listing = require("../models/listing.js");
+const { isLoggedIn } = require("../middleware.js");
 
 const validateListing = (req, res, next) => {
   let {error} = listingSchema.validate(req.body);
@@ -22,26 +23,32 @@ router.get("/", wrapAsync( async (req,res) => {
 }))
 
 //New Route
-router.get("/new", (req, res) => {
+router.get("/new",isLoggedIn, (req, res) => {
+  
   res.render("listings/new.ejs")
 })
 
 //Show Route
 router.get("/:id", wrapAsync( async (req, res) => {
   let {id} = req.params;
-  const listing = await Listing.findById(id).populate("reviews");
+  const listing = await Listing.findById(id).populate("reviews").populate("owner");
   if(!listing) {
     req.flash("error", "Listing you requested for does not exist");
     res.redirect("/listings");
     return
   }
+  console.log(listing)
   res.render("listings/show.ejs", {listing})
 }))
 
 //Create Route
-router.post("/",validateListing, wrapAsync( async (req, res, next) => {
+router.post("/",
+  isLoggedIn,
+  validateListing,
+  wrapAsync( async (req, res, next) => {
 
     const newListing = new Listing(req.body.listing);
+    newListing.owner = req.user._id;
     await newListing.save();
     req.flash("success", "New Listing Created");
     res.redirect("/listings");
@@ -49,7 +56,7 @@ router.post("/",validateListing, wrapAsync( async (req, res, next) => {
 }))
 
 //Edit Route
-router.get("/:id/edit", wrapAsync( async (req, res) => {
+router.get("/:id/edit",isLoggedIn, wrapAsync( async (req, res) => {
   let {id} = req.params;
   const listing = await Listing.findById(id);
   if(!listing) {
@@ -61,7 +68,9 @@ router.get("/:id/edit", wrapAsync( async (req, res) => {
 }))
 
 //Update Route
-router.put("/:id",validateListing, wrapAsync( async (req, res) => {
+router.put("/:id",
+  isLoggedIn,
+  validateListing, wrapAsync( async (req, res) => {
   let {id} = req.params;
   await Listing.findByIdAndUpdate(id, {...req.body.listing});
   req.flash("success", "Listing Updated");
@@ -69,7 +78,9 @@ router.put("/:id",validateListing, wrapAsync( async (req, res) => {
 }))
 
 //Delete Route
-router.delete("/:id", wrapAsync( async (req, res) => {
+router.delete("/:id", 
+  isLoggedIn,
+  wrapAsync( async (req, res) => {
   let {id} = req.params;
   let deletedListing = await Listing.findByIdAndDelete(id);
   req.flash("success", "Listing Deleted!");
