a6b3c269c95b52d43d9c2cb7d3cb8c3a3120f12c
shidhesh10 <shidheshbhatiya1004@gmail.com>
2025-12-18 21:08:05 +0530

    feat: implement MVC architecture, Cloudinary image upload, and star ratings
    Refactor & Architecture:
    - Restructured Listings, Reviews, and Users into a professional MVC (Model-View-Controller) pattern.
    - Optimized routing logic using Router.route to reduce code redundancy.
    
    Image Upload Integration:
    - Replaced manual URL input with file upload functionality using Multer.
    - Integrated Cloudinary for cloud image storage.
    - Updated Mongoose schema to store Cloudinary image URLs and filenames.
    - Added image preview functionality in the 'Edit Listing' form to show current images.
    
    UI & Reviews:
    - Enhanced Review UI: Converted numeric range input to a dynamic star rating system.
    - Star ratings are now visually rendered on the listing details page.

--- routes/review.js
+++ routes/review.js
@@ -5,28 +5,12 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const Listing = require("../models/listing.js");
 const {validateReview, isLoggedIn,isReviewAuthor } = require("../middleware.js");
 
+const reviewController = require("../controllers/reviews.js")
 
 //Post Route
-router.post("/", isLoggedIn,validateReview, wrapAsync( async (req, res)=> {
-  let listing = await Listing.findById(req.params.id);
-
-  let newReview = new Review(req.body.review);
-  newReview.author = req.user._id;
-  listing.reviews.push(newReview);
-  console.log(newReview);
-  await newReview.save();
-  await listing.save();
-  req.flash("success", "New review Created")
-  res.redirect(`/listings/${listing._id}`);
-}))
+router.post("/", isLoggedIn,validateReview, wrapAsync(reviewController.createReview));
 
 //Delete Review Route
-router.delete("/:reviewId",isLoggedIn,isReviewAuthor, wrapAsync( async (req, res) => {
-  let {id, reviewId} = req.params;
-  await Listing.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});
-  await Review.findByIdAndDelete(reviewId);
-  req.flash("success", "Review Deleted!");
-  res.redirect(`/listings/${id}`)
-}))
+router.delete("/:reviewId",isLoggedIn,isReviewAuthor, wrapAsync(reviewController.destroyReview))
 
 module.exports = router;
